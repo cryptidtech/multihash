92477258aab8afcba83f220b5cd67f78c6330170
Dave Huseby <dwh@linuxprogrammer.org>
2023-11-27 19:38:05 -0700

    fix serde/ser.rs for readable
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/serde/ser.rs
+++ src/serde/ser.rs
@@ -10,8 +10,8 @@ impl ser::Serialize for Multihash {
     {
         if serializer.is_human_readable() {
             let mut ss = serializer.serialize_struct(SIGIL.as_str(), 2)?;
-            let _ = ss.serialize_field("codec", &self.codec.code());
-            let _ = ss.serialize_field("hash", &Varbytes::encoded_new(self.hash.clone()))?;
+            ss.serialize_field("codec", &self.codec.code())?;
+            ss.serialize_field("hash", &Varbytes::encoded_new(self.hash.clone()))?;
             ss.end()
         } else {
             (SIGIL, self.codec, Varbytes(self.hash.clone())).serialize(serializer)
