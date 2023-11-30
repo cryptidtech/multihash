d3ccf0e64a705abe197849c976ea1632617dfb83
Dave Huseby <dwh@linuxprogrammer.org>
2023-11-29 19:39:12 -0700

    update rust-multicodec dependency
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -17,9 +17,9 @@ mod tests {
             &mh.compact(), // convert to Tagged<MultihashImpl>
             &[
                 Token::Tuple { len: 3 },
-                Token::Bytes(&[0x31]), // Codec::Multihash as varuint
-                Token::Bytes(&[0xE0, 0xE4, 0x02]), // Codec::Blake2S256 as varuint
-                Token::Bytes(&[
+                Token::BorrowedBytes(&[0x31]), // Codec::Multihash as varuint
+                Token::BorrowedBytes(&[0xE0, 0xE4, 0x02]), // Codec::Blake2S256 as varuint
+                Token::BorrowedBytes(&[
                     // Varbytes, which is a varuint encoded len followed by bytes
                     0x20, 0x64, 0x22, 0x03, 0x12, 0x5d, 0x59, 0xe8, 0xb9, 0x3e, 0xdb, 0x67, 0x6f,
                     0xc7, 0x8d, 0xe9, 0xc5, 0x87, 0xcf, 0x52, 0xcc, 0xc6, 0xf2, 0x19, 0x03, 0x2d,
@@ -54,13 +54,15 @@ mod tests {
             &mh.readable(),
             &[
                 Token::Struct {
-                    name: "Multihash",
+                    name: "multihash",
                     len: 2,
                 },
-                Token::Str("codec"),
+                Token::BorrowedStr("codec"),
                 Token::U64(0xb260_u64),
-                Token::Str("hash"),
-                Token::Str("f20642203125d59e8b93edb676fc78de9c587cf52ccc6f219032da1f377082332b0"),
+                Token::BorrowedStr("hash"),
+                Token::BorrowedStr(
+                    "f20642203125d59e8b93edb676fc78de9c587cf52ccc6f219032da1f377082332b0",
+                ),
                 Token::StructEnd,
             ],
         );
