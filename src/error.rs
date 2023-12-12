d6aaca2c078a9acd524cb263e3eacc9dea60f0a6
Dave Huseby <dwh@linuxprogrammer.org>
2023-12-12 13:46:17 -0700

    clean up Error
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,16 @@
-use thiserror::Error;
-
 /// Errors created by this library
-#[derive(Clone, Debug, Eq, Error, PartialEq)]
+#[derive(Clone, Debug, thiserror::Error)]
 #[non_exhaustive]
 pub enum Error {
-    /// A multibase conversion error
-    #[error(transparent)]
-    Multibase(#[from] multibase::Error),
-
     /// A multicodec decoding error
     #[error(transparent)]
     Multicodec(#[from] multicodec::Error),
-
     /// Multiutil error
     #[error(transparent)]
     Multiutil(#[from] multiutil::Error),
-
-    /// Multitrait error
-    #[error(transparent)]
-    Multitrait(#[from] multitrait::Error),
-
-    /// Missing sigil 0x31
-    #[error("Missing Multihash sigil")]
-    MissingSigil,
-
     /// Missing hash data
     #[error("Missing hash data")]
     MissingHash,
-
     /// Error with the hash scheme
     #[error("Unsupported hash algorithm: {0}")]
     UnsupportedHash(multicodec::Codec),
